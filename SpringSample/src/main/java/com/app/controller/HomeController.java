e98d7787a9e68f55341e7ddfa5728e79715d0003
youngjae124 <jyj1579@naver.com>
2026-01-21 16:10:25 +0900

    스프링 프로젝트 초기세팅 & gitignore 설정

---
+++ SpringSample/src/main/java/com/app/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+
+		//return "/WEB-INF/views/home.jsp";
+		//return "/WEB-INF/views/mainpage.jsp";
+
+		//--ViewResolver 설정 이후
+		return "home";
+	}
+}
